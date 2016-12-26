aedfe0ad8d5dc665fb31f123be8a583eac7e8f75
Jefferson Gouveia <jeffersongouveia1@hotmail.com>
2016-12-26 16:59:39 -0400

    Página de login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,91 +5,40 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/app.css">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <title>Mnemosine</title>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
+      <div class="container centralizar-login">
+        <div class="row">
+          <div class="col-xs-12">
+            <h3>LOGO</h3>
+            <form class="form-horizontal" method="post">
+              <div class="form-group">
+                <input type="email" class="form-control" name="email" placeholder="Seu e-mail" required>
+              </div>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+              <div class="form-group">
+                <input type="password" class="form-control" name="password" placeholder="Sua senha mestra" required>
+              </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+              <div class="form-group">
+                <label>
+                  <input type="checkbox"> Lembrar e-mail
+                </label>
+              </div>
+
+              <div class="form-group">
+                  <button type="submit" class="btn btn-primary">
+                    <img src="img/locked-padlock.png" alt="Cadeado">
+                    Abrir cofre
+                  </button>
+              </div>
+            </form>
+          </div>
         </div>
+      </div>
     </body>
 </html>
